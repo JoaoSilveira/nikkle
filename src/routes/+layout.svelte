b20aabb665cc1d9bd75e9f605b006d630cfad7e9
JoaoSilveira <joao.pedro0842@hotmail.com>
2024-07-24 22:18:24 -0300

    Start working on page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<div class="content">
+    <div class="navbar">
+        <a href="/daily">daily</a>
+        <a href="/infinite">infinite</a>
+    </div>
+
+    <slot />
+</div>
+
+<style lang="scss">
+    @import "../style/global.scss";
+
+    .content {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .navbar {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        max-width: 300px;
+        width: 100vw;
+        align-self: center;
+        padding-block: 32px;
+        
+        &>a {
+            text-decoration: none;
+            font-size: 2rem;
+        }
+    }
+</style>
